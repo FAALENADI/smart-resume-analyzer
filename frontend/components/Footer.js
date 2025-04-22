9c1657f004c3916ecee9957c519a7daf60082071
Fawaz Al Enadi <fawaz.alenadi@gmail.com>
2025-04-22 18:47:41 +0300

    Create Footer.js

---
+++ frontend/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center p-4 mt-8 text-sm text-gray-600">
+      Made with 💙 by Fawaz Al Enadi | <a href="https://github.com/FAALENADI" className="text-blue-600">GitHub</a>
+    </footer>
+  );
+}
